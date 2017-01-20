f82807c16c9dcbc115b0da39cc0165b5e604807b
Justin Mah <jmmah98@gmail.com>
2017-01-19 18:31:38 -0700

    Optimizations. Colour changes (Menu to blue, game start to white)

--- app/src/main/java/com/jmmxp/android/reactiontime/MainActivity.java
+++ app/src/main/java/com/jmmxp/android/reactiontime/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
@@ -25,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private AlphaAnimation mFadeOut = new AlphaAnimation(1.0f, 0.0f);
     private AlphaAnimation mFadeIn = new AlphaAnimation(0.0f, 1.0f);
     private boolean mGameStart = false;
+    private boolean mGameOver = false;
     private int mRandomTime;
+    private int mTransitionTime = 500;
 
 
     @Override
@@ -50,11 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
                     mGameStart = true;
 
-                    mInstructionTextView.startAnimation(mFadeOut);
-                    mFadeOut.setDuration(1000);
-                    mFadeOut.setFillAfter(true);
+                    animateTextOut();
 
-                    mTransitionDrawable.startTransition(1000);
+                    mTransitionDrawable.startTransition(mTransitionTime);
                     final Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         @Override
@@ -62,12 +63,34 @@ public class MainActivity extends AppCompatActivity {
                             onTimeReached();
                         }
                     }, mRandomTime);
-                } else {
-                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(intent);
-                    overridePendingTransition(0, 0);
                 }
+
+                mMainLinearLayout.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        mMainLinearLayout.setOnTouchListener(null);
+                        mGameOver = true;
+                        Log.d(TAG, "Game over");
+//                        mMainLinearLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.startGame));
+                        mMainLinearLayout.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.colour_transition3));
+                        mTransitionDrawable = (TransitionDrawable) mMainLinearLayout.getBackground();
+                        mTransitionDrawable.startTransition(mTransitionTime);
+
+                        animateTextIn("You tapped too early! Try again.");
+
+                        final Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                setResetOnClickListener();
+                            }
+                        }, mTransitionTime);
+
+
+                        return true;
+
+                    }
+                });
 
             }
         });
@@ -76,33 +99,69 @@ public class MainActivity extends AppCompatActivity {
 
     public void onTimeReached() {
 
-        final long startTime = SystemClock.elapsedRealtime();
+        if (!mGameOver) {
 
-        mMainLinearLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.promptClick));
-        mMainLinearLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.colour_transition2));
-        mTransitionDrawable = (TransitionDrawable) mMainLinearLayout.getBackground();
+            final long startTime = SystemClock.elapsedRealtime();
 
+            mMainLinearLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.promptClick));
+            mMainLinearLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.colour_transition2));
+            mTransitionDrawable = (TransitionDrawable) mMainLinearLayout.getBackground();
+
+            mMainLinearLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    mMainLinearLayout.setOnTouchListener(null);
+                    Log.d(TAG, "Game over");
+
+                    long endTime = SystemClock.elapsedRealtime();
+                    long elapsedMilliseconds = endTime - startTime;
+                    // double elapsedSeconds = elapsedMilliSeconds / 1000.0;
+
+                    mTransitionDrawable.startTransition(mTransitionTime);
+
+                    animateTextIn(elapsedMilliseconds + " milliseconds");
+
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            setResetOnClickListener();
+                        }
+                    }, mTransitionTime);
+
+                    return true;
+
+                }
+            });
+
+        }
+
+    }
+
+    private void setResetOnClickListener() {
+        mMainLinearLayout.setOnTouchListener(null);
         mMainLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Game over");
-
-                long endTime = SystemClock.elapsedRealtime();
-                long elapsedMilliSeconds = endTime - startTime;
-                double elapsedSeconds = elapsedMilliSeconds / 1000.0;
-
-                mTransitionDrawable.startTransition(1000);
-
-                mInstructionTextView.startAnimation(mFadeIn);
-                mFadeIn.setDuration(1000);
-                mFadeIn.setFillAfter(true);
-
-                mInstructionTextView.setText(elapsedSeconds + " seconds");
-
-
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
             }
         });
+    }
 
+    private void animateTextIn(CharSequence text) {
+        mInstructionTextView.startAnimation(mFadeIn);
+        mFadeIn.setDuration(mTransitionTime);
+        mFadeIn.setFillAfter(true);
+        mInstructionTextView.setText(text);
+    }
+
+    private void animateTextOut() {
+        mInstructionTextView.startAnimation(mFadeOut);
+        mFadeOut.setDuration(mTransitionTime);
+        mFadeOut.setFillAfter(true);
     }
 
 }
